c252533952dd512d7baf104580e0aa2d37f299cc
alvaro_perez <apg2908@gmail.com>
2020-09-18 13:18:59 +0200

    Second commit

--- src/sum.spec.js
+++ src/sum.spec.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 3 + 2 to equal 5', () => {
   expect(sum(3, 2)).toBe(5);
 });
+
+test('adds 3 + 6 to equal 9', () => {
+  expect(sum(3, 6)).toBe(9);
+});
