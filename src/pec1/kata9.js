ab675ec9e51845c756dd081600e715b91dd2d4f6
alvaro_perez <apg2908@gmail.com>
2020-10-01 18:54:00 +0200

    Kata 9

--- src/pec1/kata9.js
+++ src/pec1/kata9.js
@@ -10,3 +10,30 @@ export default function plantTree(species, fruit) {
   }
   return null;
 }
+
+// For exercises 9.3 and 9.4
+export function plantTreeWithGetters(species, fruit) {
+  let mySpecie = species;
+  let myFruit = fruit;
+  if (typeof mySpecie === 'string' && typeof myFruit == 'string') {
+    return {
+      getSpecies() {
+        return mySpecie;
+      },
+      getFruit() {
+        return myFruit;
+      },
+      setSpecies(species) {
+        if (typeof species === 'string') {
+          mySpecie = species;
+        }
+      },
+      setFruit(fruit) {
+        if (typeof fruit === 'string') {
+          myFruit = fruit;
+        }
+      },
+    };
+  }
+  return null;
+}
--- src/pec1/kata9.spec.js
+++ src/pec1/kata9.spec.js
@@ -1,4 +1,5 @@
 import plantTree from './kata9';
+import { plantTreeWithGetters } from './kata9';
 
 describe('Kata #9: factoría de objetos', () => {
   // 9.1
@@ -36,5 +37,59 @@ describe('Kata #9: factoría de objetos', () => {
 
   test('plantTree getFruit method returns "pear"', () => {
     expect(plantTree('pearTree', 'pear').getFruit()).toBe('pear');
+  });
+
+  // 9.3
+  test('plantTreeWithGetters has property getFruit', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).toHaveProperty('getFruit');
+  });
+
+  test('plantTreeWithGetters has not property fruit', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).not.toHaveProperty('fruit');
+  });
+
+  test('plantTreeWithGetters has property getSpecies', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).toHaveProperty('getSpecies');
+  });
+
+  test('plantTreeWithGetters has not property species', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).not.toHaveProperty('species');
+  });
+
+  test('plantTreeWithGetters getFruit method returns "pear"', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear').getFruit()).toBe('pear');
+  });
+
+  test('plantTreeWithGetters getSpecies method returns "pearTree"', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear').getSpecies()).toBe('pearTree');
+  });
+
+  //9.4
+  test('plantTreeWithGetters has property getFruit', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).toHaveProperty('getFruit');
+  });
+
+  test('plantTreeWithGetters has property getSpecies', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).toHaveProperty('getSpecies');
+  });
+
+  test('plantTreeWithGetters has property setFruit', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).toHaveProperty('setFruit');
+  });
+
+  test('plantTreeWithGetters has property setSpecies', () => {
+    expect(plantTreeWithGetters('pearTree', 'pear')).toHaveProperty('setSpecies');
+  });
+
+  test('plantTreeWithGetters setFruit does not set value when it is not a string', () => {
+    var tree = plantTreeWithGetters('pearTree', 'pear');
+    tree.setFruit(12);
+    expect(tree.getFruit()).toBe('pear');
+  });
+
+  test('plantTreeWithGetters setFruit set frui value when it is a string', () => {
+    var tree = plantTreeWithGetters('pearTree', 'pear');
+    tree.setFruit('apple');
+    expect(tree.getFruit()).toBe('apple');
   });
 });
